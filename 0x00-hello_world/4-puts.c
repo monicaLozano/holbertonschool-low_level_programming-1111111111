5400dcecd371a1aa77a698e34e099e39be215552
monicaLozano <3367@holbertonscho.com>
2021-06-17 15:05:37 -0700

    mi primer C

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,9 @@
 #include <studio.h>
 
 /*Escribir con C*/
+
 init main(void)
 {
-  puts("Programming is like building a multilingual puzzle");
-  return 0;
+puts("Programming is like building a multilingual puzzle");
+return (0);
 }
