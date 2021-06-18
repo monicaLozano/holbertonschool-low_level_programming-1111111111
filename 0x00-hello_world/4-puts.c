68405791292d9fb366ef9bc5b2c9aae143e2ebce
monicaLozano <3367@holbertonscho.com>
2021-06-17 17:06:49 -0700

    c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,6 @@
 #include <stdio.h>
 int main(void)
-/*
- * main - funcion principal
-*/
+/* main - funcion principal */
 {
 puts("\"Programming is like building a multilingual puzzle");
 return (0);
