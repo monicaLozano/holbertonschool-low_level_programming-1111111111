60bbfc58734f1a398d75247a4b7c04e284fe9417
monicaLozano <3367@holbertonscho.com>
2021-06-18 12:56:26 -0700

    positive

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -5,16 +5,20 @@
 /* betty style doc for function main goes there */
 int main(void)
 {
-int n;
-srand(time(0));
-n = rand() - RAND_MAX / 2;
-if(n == 0){
-printf("is zero");
-}else if(n > 0){
-printf("is positive");
-}else{
-// Si no es neutro ni positivo, es negativo
-printf("is negative");
-/* your code goes there */
-  return (0);
+  int n;
+
+  srand(time(0));
+  n = rand() - RAND_MAX / 2;
+  /* your code goes there */
+
+  if(n > 0){
+    printf("is positive");
+  
+  }else if(n == 0){
+    printf("is zero");
+  
+  }else if(n < 0)
+    printf("is negative");
+
+return (0);
 }
