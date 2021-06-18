a181641c451a4038b94bb8360083e232cb75fe89
monicaLozano <3367@holbertonscho.com>
2021-06-18 13:14:31 -0700

    positive

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -13,15 +13,15 @@ int main(void)
 
   if (n > 0)
     {
-     printf("is positive");
+      printf("is positive %d\n", n);
     }
   else if(n == 0)
     {
-     printf("is zero");
+      printf("is zero %d\n", n);
     }
   else
     {
-     printf("is negative");
+      printf("is negative %d\n", n);
     }
 return (0);
 }
