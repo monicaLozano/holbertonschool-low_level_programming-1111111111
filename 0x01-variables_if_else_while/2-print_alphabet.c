2c18f1c33d0d6d19eda49c3589b9230278780db3
monicaLozano <3367@holbertonscho.com>
2021-06-18 17:27:28 -0700

    abcdario

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -11,12 +11,13 @@ int main(void)
 int n;
 srand(time(0));
 n = rand() - RAND_MAX / 2;
-char c;
-putchar("Ingrese un caracter en mayúscula: ");
- scanf("%c", &c);
-if (c >= 'A' && c <= 'Z')
-   c = c - 'A' + 'a';
-putchar("En minúscula es: %c\n", c);
+ char m = 'a';
+ while (m <= 'z')
+{
+putchar(m);
+m++;
+}
+ putchar('\n');
 }
 return (0);
 }
