d3367a0ba9e4fec61e8fd437c979506cbdb8dc0d
monicaLozano <3367@holbertonscho.com>
2021-06-18 18:49:24 -0700

    Aa

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -1,20 +1,28 @@
 #include <stdlib.h>
 #include <time.h>
 #include <stdio.h>
+#include <ctype.h>
 /**
-* main - positive negative zero
-*
-* Return: always zero
+ * main - Determine if the last digit of a random number is
+ * greater than or less than 5, or is zero..
+(*
+* Return: 0 on success
 */
 int main(void)
 {
-char i;
-for (i = 'a' ; i <= 'z' ; i++)
-putchar(i);
-char j;
-for (i = 'A' ; i <= 'Z' ; i++)
+char i = 'a';
+char j = 'z';
+while (i <= j)
 {
-putchar(J);
+putchar(i);
+i++;
+}
+i = 'a';
+j = 'z';
+while (i <= j)
+{
+putchar(toupper(i));
+i++;
 }
 putchar('\n');
 return (0);
