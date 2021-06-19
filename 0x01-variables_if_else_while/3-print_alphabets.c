d7da36e3b4f008b2c0834667c6c243b690d99eea
monicaLozano <3367@holbertonscho.com>
2021-06-18 18:00:02 -0700

    Aa

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,22 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+#include <ctype.h>
+/**
+* main - positive negative zero
+*
+* Return: always zero
+*/
+int main(void)
+{
+char i;
+char j;
+for (i = 'a' ; i <= 'z' ; i++)
+for (i = 'A' ; i <= 'Z' ; i++)
+{
+putchar(i);
+putchar(J);
+}
+putchar('\n');
+return (0);
+}
---
+++ 0x01-variables_if_else_while/3-print_alphabets.c~
@@ -0,0 +1,18 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+* main - positive negative zero
+*
+* Return: always zero
+*/
+int main(void)
+{
+char i;
+for (i = 'a' ; i <= 'z' ; i++)
+{
+putchar(i);
+}
+putchar('\n');
+return (0);
+}
