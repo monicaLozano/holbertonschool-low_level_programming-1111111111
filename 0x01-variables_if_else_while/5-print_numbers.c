b990a2c9e38b12b3ef6c672467341e5036cd4701
monicaLozano <3367@holbertonscho.com>
2021-06-18 19:39:19 -0700

    abc

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,22 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+#include <ctype.h>
+/**
+ * main - Determine if the last digit of a random number is
+ * greater than or less than 5, or is zero..
+(*
+* Return: 0 on success
+*/
+int main(void)
+{
+char i = 0;
+while (i < 10)
+{
+printf("%d", i);
+}
+i++;
+}
+putchar('\n');
+return (0);
+}
