fc11bd85d9a2fd92c3fdbfc08268b73c0929d2d5
monicaLozano <3367@holbertonscho.com>
2021-06-22 15:17:03 -0700

    print_sign

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -1,14 +1,14 @@
 #include "holberton.h"
 /**
- * main - check the code for Holberton School students.
- *
- * Return: Always 0.
- */
+* print_sign - character to be checked if alpha
+*
+* Return: 1 if letter, 0 otherwise
+**/
 int print_sign(int n);
 {
 if (n > 0)
 {
- _putchar('+');
+_putchar('+');
 return (1);
 }
 else if (n == 0)
@@ -16,9 +16,9 @@ else if (n == 0)
 _putchar (0)
 return (0);
 }
-else 
+else
 {
 _putchar (-)
-return (-1)   
+return (-1)
 }
 }
