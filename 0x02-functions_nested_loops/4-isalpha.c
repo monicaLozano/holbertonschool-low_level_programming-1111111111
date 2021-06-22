1fab00e141903c4b6f73f6c83043ea84f6bf4a2b
monicaLozano <3367@holbertonscho.com>
2021-06-22 15:08:34 -0700

    a

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,12 +1,14 @@
 #include "holberton.h"
 /**
-* int _isalpha(int c)  - Check alphabetic
-* @c: character
-* Return: 0
-*/
+* _isalpha - returns 1 if c is a letter
+*
+* @c: character to be checked if alpha
+*
+* Return: 1 if letter, 0 otherwise
+**/
 int _isalpha(int c)
 {
-if (c >= 'a' && c <= 'z')
+if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
 return (1);
 else
 return (0);
