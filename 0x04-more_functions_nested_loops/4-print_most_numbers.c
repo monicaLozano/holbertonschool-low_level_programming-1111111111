1e6154e0dac5e2f7f28e365b5d8cfbb61b6ff7eb
monicaLozano <3367@holbertonscho.com>
2021-06-24 21:12:01 -0700

    ja

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -1,14 +1,14 @@
 #include "holberton.h"
 
 /**
-* print_most_numbers - check the code for Holberton School students.
-*
-* Return: Always 0.
+ * print_most_numbers - check the code for Holberton School students.
+ *
+ * Return: Always 0.
 */
 
 void print_most_numbers(void)
 {
-int num;
+ int num;
 
 for (num = 48 ; num <= 57; num++)
 {
