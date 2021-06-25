6486c6f052dd2d7fa45877ff94f785e42fb330f5
monicaLozano <3367@holbertonscho.com>
2021-06-24 21:21:32 -0700

    print square

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,21 @@
+#include "holberton.h"
+/**
+ * print_square - check the code for Holberton School students.
+ *
+ * @size: calculate
+ */
+void print_square(int size)
+{
+
+	int a, b;
+
+	if (size <= 0)
+		_putchar(10);
+	else
+		for (a = 0; a < size; a++)
+			{
+				for (b = 0; b < size; b++)
+					_putchar(35);
+				_putchar(10);
+			}
+}
