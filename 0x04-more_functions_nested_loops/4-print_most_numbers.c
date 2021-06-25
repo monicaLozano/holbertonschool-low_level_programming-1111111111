c333703cb02b105c8af55bd70033bfd2b2913cca
monicaLozano <3367@holbertonscho.com>
2021-06-25 09:33:02 -0700

    most number

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -1,20 +1,20 @@
 #include "holberton.h"
 
 /**
- * print_most_numbers - check the code for Holberton School students.
- * new line
- */
+* print_most_numbers - check the code for Holberton School students.
+* new line
+*/
 void print_most_numbers(void)
 {
-	int c;
+int c;
 
-	for (c = 0; c < 10; c++)
-		{
-			if (c != 2 && c != 4)
-				{
-					_putchar(c + '0');
-				}
-		}
+for (c = 0; c < 10; c++)
+{
+if (c != 2 && c != 4)
+{
+_putchar(c + '0');
+}
+}
 
-	_putchar('\n');
+_putchar('\n');
 }
