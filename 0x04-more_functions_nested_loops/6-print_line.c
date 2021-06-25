b81ba38cc300476294233b3334295b3ee47cb42d
monicaLozano <3367@holbertonscho.com>
2021-06-24 20:44:18 -0700

    printi line

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,16 @@
+#include "holberton.h"
+/**
+* print_line - check the code for Holberton School students.
+*
+* Return: Always 0.
+*/
+void print_line(int n)
+{
+int n + 1;
+while (a > 0)
+{
+_putchar (95);
+n--;
+}
+_putchar (10);
+}
