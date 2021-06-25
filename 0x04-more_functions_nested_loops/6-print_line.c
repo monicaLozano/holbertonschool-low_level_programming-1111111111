e5e4b0dfe6d2d7ce43cd31b7b2402e581b4124aa
monicaLozano <3367@holbertonscho.com>
2021-06-24 20:51:02 -0700

    moni

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -6,13 +6,13 @@
 */
 void print_line(int n)
 {
-	if (n <= 0)
-		{
-			_putchar('\n');
-		}
-	else
-		{
-			_putchar('_');
-			print_line(n - 1);
-		}
+if (n <= 0)
+{
+_putchar('\n');
+}
+else
+{
+_putchar('_');
+print_line(n - 1);
+}
 }
