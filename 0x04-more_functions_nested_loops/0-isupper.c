02d35b346be0c72cc6cff0e314590bf95085b1a2
monicaLozano <3367@holbertonscho.com>
2021-06-24 18:28:55 -0700

    super

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,16 +1,17 @@
+#include "holberton.h"
 /**
- * _isupper; checks whether a character is an uppercase alphabet (A-Z) or not.
- * @c: comparate
- * Return: 1  o 0
- */
+* _isupper - checks whether a character is an uppercase alphabet (A-Z) or not.
+* @c: comparate charter.
+* Return: Always 0.
+*/
 int _isupper(int c)
 {
-	if (c >= 'A' && c <= 'Z')
-	{
-		return (1);
-	}
-	else
-	{
-		return (0);
-	}
+if (c >= 'A' && c <= 'Z')
+{
+return (1);
+}
+else
+{
+return (0);
+}
 }
