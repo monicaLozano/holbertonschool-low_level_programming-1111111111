aaac89c9d5df5295653ad74a01c3e557133da232
monicaLozano <3367@holbertonscho.com>
2021-06-24 19:31:23 -0700

    terc

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,14 @@
+#include "holberton.h"
+/**
+* print_numbers - check the code for Holberton School students.
+*
+* Return: Always 0.
+*/
+void print_numbers(void)
+{
+int x;
+for (x = 0; x <= 9; x++)
+_putchar('0' + x);
+}
+_putchar ('\n');
+}
