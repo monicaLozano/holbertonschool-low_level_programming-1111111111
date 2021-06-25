f20ba6a0f86b7ca102f67f0378e22619e8c69f5a
monicaLozano <3367@holbertonscho.com>
2021-06-24 21:02:22 -0700

    print7

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -1,28 +1,28 @@
 #include "holberton.h"
 /**
- * print_diagonal - check the code for Holberton School students
- * @n: check the code for Holberton School students
- *
- * Return: a diagonal
- */
+* print_diagonal - check the code for Holberton School students
+* @n: check the code for Holberton School students
+*
+* Return: a diagonal
+*/
 void print_diagonal(int n)
 {
-	int a, b;
+int a, b;
 
-	if (n > 0)
-		{
-			for (a = 0 ; a < n ; a++)
-				{
-					for (b = 0 ; b < a ; b++)
-						{
-							_putchar(' ');
-						}
-					_putchar(92);
-					_putchar('\n');
-				}
-		}
-	else
-		{
-			_putchar('\n');
-		}
+if (n > 0)
+{
+for (a = 0 ; a < n ; a++)
+{
+for (b = 0 ; b < a ; b++)
+{
+_putchar(' ');
+}
+_putchar(92);
+_putchar('\n');
+}
+}
+else
+{
+_putchar('\n');
+}
 }
