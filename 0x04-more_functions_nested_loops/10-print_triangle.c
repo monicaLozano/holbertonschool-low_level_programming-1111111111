c88fad6a6be9c393c921117c93f5be7eddb53095
monicaLozano <3367@holbertonscho.com>
2021-06-25 18:53:28 -0700

    size

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -1,23 +1,21 @@
 #include "holberton.h"
 /**
-* print_triangle - check the code for Holberton School
-*
-* @size: Always 0.
-*/
-
+ * print_triangle - check the code for Holberton School students.
+ * @size: calculare
+ */
 void print_triangle(int size)
 {
-{
-int a, b, c;
-if (size <= 0)
-_putchar('\n');
-else
-for (a = 0; a < size; a++)
-{
-for (b = size; b >= a + 2; b--)
-_putchar(' ');
-for (c = 0; c < a + 1; c++)
-_putchar('#');
-_putchar('\n');
-}
+	int a, b, c;
+
+	if (size <= 0)
+		_putchar('\n');
+	else
+		for (a = 0; a < size; a++)
+			{
+				for (b = size; b >= a + 2; b--)
+					_putchar(' ');
+				for (c = 0; c < a + 1; c++)
+					_putchar('#');
+				_putchar('\n');
+			}
 }
