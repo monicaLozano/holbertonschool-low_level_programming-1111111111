1f7084c548def4f5dc8bb10fe1083701cd34f794
monicaLozano <3367@holbertonscho.com>
2021-06-28 15:42:42 -0700

    reset

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,13 @@
+#include "holberton.h"
+#include <stdio.h>
+
+/**
+* reset_to_98 - this function modify n
+* @n: is the modifi param
+* Return: Always 0.
+*/
+void reset_to_98(int *n)
+{
+int n;
+*n = 98;
+}
