57d8a518f4dd459a379173999693cfbef3e8fb9f
monicaLozano <3367@holbertonscho.com>
2021-06-28 19:47:43 -0700

    pot

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -1,8 +1,8 @@
 #include "holberton.h"
 #include <stdio.h>
 /**
- * main - check the code for Holberton School students.
- *
+ * _puts - check the code for Holberton School students.
+ * @str: funtion
  * Return: Always 0.
  */
 
