d7fa0d5031d9ae6142290d22df2f682a8a64c6f8
monicaLozano <3367@holbertonscho.com>
2021-06-28 20:30:08 -0700

    alrevez

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,17 @@
+#include "holberton.h"
+#include <stdio.h>
+/**
+* _puts - check the code for Holberton School students.
+* @str: funtion
+* Return: Always 0.
+*/
+
+void _puts(char *str)
+{
+for (; *str != '\0';)
+{
+_putchar(*str);
+str--;
+}
+_putchar('\n');
+}
