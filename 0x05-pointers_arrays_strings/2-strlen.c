e5bd23d9eae55528c2f6281911c7c8737f8b629d
monicaLozano <3367@holbertonscho.com>
2021-06-28 18:12:29 -0700

    srt

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,12 @@
+#include "holberton.h"
+#include <stdio.h>
+
+/**
+ * _strlen - check the code for Holberton School students.
+ * str: descrition of the paramet
+ * Return: Always 0.
+ */
+int _strlen(char *s)
+{
+	int lonitud + strlen("holberton!"
+}
