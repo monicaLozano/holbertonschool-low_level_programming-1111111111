545daf68b9384a8d3c1f3323e46d72688eaec4b9
monicaLozano <3367@holbertonscho.com>
2021-06-28 20:24:08 -0700

    str

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -1,13 +1,14 @@
 #include "holberton.h"
 #include <stdio.h>
 /**
- * _puts - check the code for Holberton School students.
- * @str: funtion
- * Return: Always 0.
- */
+* _puts - check the code for Holberton School students.
+* @str: funtion
+* Return: Always 0.
+*/
+
 void _puts(char *str)
 {
-	for (; *str != '\0';)
+for (; *str != '\0';)
 {
 _putchar(*str);
 str++;
