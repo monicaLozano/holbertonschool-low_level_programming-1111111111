43f6fdfae1044eb859b9073f5ac447a5d9cc50fb
monicaLozano <3367@holbertonscho.com>
2021-06-28 18:27:44 -0700

    str

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -3,16 +3,16 @@
 
 /**
  * _strlen - check the code for Holberton School students.
- * str: descrition of the paramet
+ * @s: descrition of the paramet
  * Return: Always 0.
  */
 int _strlen(char *s)
 {
 int len = 0;
-while(*s != 0)
+while (*s != 0)
 {
 s++;
 len++;
 }
-return len;
+return (len)
 }
