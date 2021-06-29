5d851f3a9f8d1c9426abe32fd86af7195768a15f
monicaLozano <3367@holbertonscho.com>
2021-06-28 17:30:40 -0700

    swap

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,18 @@
+#include "holberton.h"
+
+/**
+ * swap_int - check the code for Holberton School students.
+ *
+ * Return: Always 0.
+ */
+void swap_int(int *a, int *b)
+{
+*a = 98;
+*b = 42
+if (a != b)
+{
+*a += *b;
+*b = *a - *b;
+*a -= *b;
+}
+}
